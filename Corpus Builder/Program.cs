8ce07cc10e1b6ddb6987f0bbc6761077d6722ce4
Yasser Souri <yassersouri@gmail.com>
2011-04-26 16:36:41 +0430

    add functions for refining all files in a direactory

--- Corpus Builder/Program.cs
+++ Corpus Builder/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SCICT.NLP.Utility;
+using System.IO;
 
 namespace Corpus_Builder
 {
@@ -9,6 +11,45 @@ namespace Corpus_Builder
     {
         static void Main(string[] args)
         {
+
+            try
+            {
+				string dir = "D:\\crawler\\sites\\mehrnews\\";
+
+
+				RefineAllFiles(dir);
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        static void RefineAllFiles(String dir)
+        {
+			string[] files = Directory.GetFiles(dir);
+			for(int i = 0; i < files.Length; i++){
+				RefineFile(files[i]);
+				Console.WriteLine("Refined file: " + files[i].ToString());
+			}
+        }
+
+        private static void RefineFile(String file)
+        {
+			string line;
+			StreamReader sr = new StreamReader(file);
+			line = sr.ReadToEnd();
+			sr.Close();
+
+			line = StringUtil.RefineAndFilterPersianWord(line);
+
+			TextWriter tw = new StreamWriter(file);
+			tw.Write(line);
+			tw.Flush();
+			tw.Close();
         }
     }
 }
