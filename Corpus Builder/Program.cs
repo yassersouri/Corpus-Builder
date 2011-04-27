7620da4858428922a97fb484121d91406953bc27
Yasser Souri <yassersouri@gmail.com>
2011-04-27 15:52:27 +0430

    add function for separating sentences in files

--- Corpus Builder/Program.cs
+++ Corpus Builder/Program.cs
@@ -15,9 +15,12 @@ namespace Corpus_Builder
             try
             {
 				string dir = "D:\\sample\\";
+				string file = "1143.txt";
+				string sourceFile = dir + file;
+				string destinationDir = dir + "corpus\\";
+				string destinationFile = destinationDir + file;
 
-				string file = dir + "1143.txt";
-				PrintAllSentenses(file);
+				SeparateAllSentences(dir, destinationDir);
 				
             }
             catch (Exception e)
@@ -28,18 +31,45 @@ namespace Corpus_Builder
             }
         }
 
-		static void PrintAllSentenses(String file)
+		static void SeparateAllSentences(String sourceDir, String destinationDir){
+			string[] sourcefiles = Directory.GetFiles(sourceDir);
+			Directory.CreateDirectory(destinationDir);
+
+			string destinationfile;
+			string sourcefile;
+			int lastIndex;
+			string fileName;
+			int len;
+
+			for(int i = 0; i < sourcefiles.Length; i++)
+			{	
+				sourcefile = sourcefiles[i];
+
+				lastIndex = sourcefile.LastIndexOf('\\');
+				len = sourcefile.ToString().Length;
+				fileName = sourcefile.ToString().Substring(lastIndex+1, len-lastIndex-1);
+
+				destinationfile = destinationDir + fileName;
+
+				SeparateSentences(sourcefile, destinationfile);
+
+				Console.WriteLine("Extracted sentences of: " + sourcefile);
+			}
+
+		}
+
+		static void SeparateSentences(String sourceFile, String destinationFile)
 		{
-			StreamReader sr = new StreamReader(file);
+			StreamReader sr = new StreamReader(sourceFile);
 			String line = sr.ReadToEnd();
 			sr.Close();
 			String[] sentences;
 			sentences = StringUtil.ExtractPersianSentences(line);
-			StreamWriter sw = new StreamWriter("test.txt");
+			StreamWriter sw = new StreamWriter(destinationFile);
 			for(int i = 0; i < sentences.Length; i++)
 			{
 				sw.WriteLine(sentences[i]);
-				Console.WriteLine(sentences[i]);
+				sw.WriteLine();
 			}
 			sw.Flush();
 			sw.Close();
