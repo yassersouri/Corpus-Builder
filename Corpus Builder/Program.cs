fbea5daa02be36f6942a1033929eb8c0561f968c
Yasser Souri <yassersouri@gmail.com>
2011-06-07 12:18:30 +0430

    add VerbInflector's Program.cs to Corpus Builder's Program as a method now all actions take place in this project

--- Corpus Builder/Program.cs
+++ Corpus Builder/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using SCICT.NLP.Utility;
 using SCICT.NLP.Persian;
 using System.IO;
+using VerbInflector;
 
 namespace Corpus_Builder
 {
@@ -12,20 +13,33 @@ namespace Corpus_Builder
     {
         static void Main(string[] args)
         {
-            try
-            {
-				string dir = "D:\\sample\\";
-				string destinationDir = dir + "corpus\\";
+			//try
+			//{
+			//    string dir = "D:\\sample\\";
+			//    string destinationDir = dir + "corpus\\";
 
-				RefineAllFiles(dir);
-				SeparateAllSentencesAndWords(dir,destinationDir);
+			//    RefineAllFiles(dir);
+			//    SeparateAllSentencesAndWords(dir,destinationDir);
 				
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("The file could not be read:");
-                Console.WriteLine(e.Message);
-            }
+			//}
+			//catch (Exception e)
+			//{
+			//    Console.WriteLine("The file could not be read:");
+			//    Console.WriteLine(e.Message);
+			//}
+
+
+			string verbDicPath = "../../VerbList.txt";
+
+			string sourceDir = "D:\\sample\\corpus_tagged\\";
+			string destinationDir = sourceDir + "again\\";
+			string file = "1088245.txt";
+
+			Directory.CreateDirectory(destinationDir);
+			string sourceFile = sourceDir + file;
+			string destinationFile = destinationDir + file;
+
+			VerbInflector_OneFile(sourceFile, verbDicPath);
         }
 
 		static WordPatternInfo[] SeparateWords(string sentence){
@@ -112,5 +126,85 @@ namespace Corpus_Builder
 			tw.Write(line);
 			tw.Close();
         }
+
+		public static void VerbInflector_OneFile(string sourceFile, string verbDicPath)
+		{
+			Article currentArticle = ArticleUtils.getArticle(sourceFile);
+
+			Article newArticle = new Article();
+
+			Sentence currentSentence = null;
+			Sentence newSentence = null;
+			String[] currentLexemes = null;
+			String[] currentPOSTags = null;
+			for (int i = 0; i < currentArticle.getSentences().Length; i++)
+			{
+				//initialize the new sentence
+				newSentence = new Sentence();
+
+				//load the current sentence
+				currentSentence = currentArticle.getSentence(i);
+
+				currentLexemes = currentSentence.getLexeme();
+				currentPOSTags = currentSentence.getPOSTag();
+
+				//test input, 
+				string[] testSentence = "به گردش درآمده است این مسائل".Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+				string[] testPos = "P N V V PR N".Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+				//currentLexemes = testSentence;
+				//currentPOSTags = testPos;
+
+				//MakePartialDependencyTree for the current sentence.
+				List<DependencyBasedToken> list = VerbPartTagger.MakePartialDependencyTree(currentLexemes, currentPOSTags, verbDicPath);
+
+				//word index pointer in the current sentence
+				int sentenceIndex = 0;
+
+				Word currentWord = null;
+				Word newWord = null;
+				DependencyBasedToken currentDBT;
+				for (int j = 0; j < list.Count(); j++)
+				{
+					currentDBT = list[j];
+
+					newWord = new Word();
+
+					newWord.num = currentDBT.Position;
+					newWord.lexeme = currentDBT.WordForm;
+					if (currentDBT.Lemma != "_")
+					{
+						//if lemma is changed
+						newWord.lemma = currentDBT.Lemma;
+						newWord.cpos = currentDBT.CPOSTag;
+						newWord.fpos = currentDBT.FPOSTag;
+						newWord.person = currentDBT.MorphoSyntacticFeats.Person.ToString();
+						newWord.number = currentDBT.MorphoSyntacticFeats.Number.ToString();
+						newWord.tma = currentDBT.MorphoSyntacticFeats.TenseMoodAspect.ToString();
+						newWord.parentId = currentDBT.HeadNumber;
+						newWord.parentRelation = currentDBT.DependencyRelation;
+					}
+					else
+					{
+						//lemma is unchanged
+						currentWord = currentSentence.getWord(sentenceIndex);
+						newWord.lemma = currentWord.lemma;
+						newWord.cpos = currentWord.cpos;
+						newWord.fpos = currentWord.fpos;
+						newWord.person = currentWord.person;
+						newWord.number = currentWord.number;
+						newWord.tma = "_";
+						newWord.parentId = currentWord.parentId;
+						newWord.parentRelation = currentWord.parentRelation;
+					}
+					newSentence.addWord(newWord);
+					sentenceIndex += currentDBT.TokenCount;
+				}
+
+				newArticle.addSentence(newSentence);
+
+				System.Console.WriteLine(list);
+
+			}
+		}
     }
 }
